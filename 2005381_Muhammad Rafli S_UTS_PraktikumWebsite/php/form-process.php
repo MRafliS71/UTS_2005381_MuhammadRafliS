01eb79119936dd408d1727de021b9cec0ec7e7e9
MRafliS71 <99678525+MRafliS71@users.noreply.github.com>
2022-04-17 23:21:30 +0700

    Add files via upload

---
+++ 2005381_Muhammad Rafli S_UTS_PraktikumWebsite/php/form-process.php
@@ -0,0 +1,66 @@
+<?php
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// Subject
+if (empty($_POST["subject"])) {
+    $errorMSG .= "Subject is required ";
+} else {
+    $subject = $_POST["guest"];
+}
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+
+$EmailTo = "armanmia7@gmail.com";
+$Subject = "New Message Received";
+
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "guest: ";
+$Body .= $guest;
+$Body .= "\n";
+$Body .= "event: ";
+$Body .= $event;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
